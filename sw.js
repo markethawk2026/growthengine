a2a1072204bb8f8918ddb49fe81156303e07b0d0
markethawk2026 <markethawk2026@gmail.com>
2026-07-10 12:56:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="nc-markets-shell-v8b-dynamic";
-const SHELL=["./","./index.html","./css/style.css","./js/api.js","./js/main.js","./js/security.js","./js/data-source.js","./js/request-manager.js","./js/user-tools.js","./js/user-tools-ui.js","./js/market-intelligence.js","./js/market-intelligence-ui.js","./js/product-polish.js","./js/live-deployment-repair.js","./manifest.webmanifest"];
+const CACHE_NAME="nc-markets-shell-v12-latest-repair";
+const SHELL=["./","./index.html","./css/style.css","./js/api.js","./js/main.js","./js/security.js","./js/data-source.js","./js/request-manager.js","./js/user-tools.js","./js/user-tools-ui.js","./js/market-intelligence.js","./js/market-intelligence-ui.js","./js/product-polish.js","./js/regression-repair.js","./js/live-deployment-repair.js","./manifest.webmanifest"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
